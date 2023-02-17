9c37dfbe4640f0501bb29c5bd86bb63428186041
Michael Gysel <michael.gysel@unblu.com>
2023-02-17 16:36:13 +0100

    import cleanup

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,7 +3,6 @@ package com.example.demo
 import io.micrometer.core.instrument.Metrics
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
