fa48a72606e5c3e1edf31ff806afdc3bcbc24e8f
Michael Gysel <michael.gysel@unblu.com>
2023-02-21 13:27:19 +0100

    Add label tag, fix index pattern

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -4,21 +4,22 @@ import io.micrometer.core.instrument.Metrics
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
 
 @RestController
 class MainController {
 
-	@GetMapping("/")
-	fun index(): String {
-		Metrics.counter("hello").increment()
-		return "Hello World"
-	}
+    @GetMapping("/")
+    fun index(@RequestParam(required = false, defaultValue = "world") name: String): String {
+        Metrics.counter("hello", "name", name).increment()
+        return "Hello $name"
+    }
 }
